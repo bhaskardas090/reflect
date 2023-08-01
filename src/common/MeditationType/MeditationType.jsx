d1e9df72d8fecda2515fee248632f4b9fd917d2a
Kumar Rohit <91978766+kumarrohit2509@users.noreply.github.com>
2023-08-01 11:18:17 +0530

    UI

--- src/common/MeditationType/MeditationType.jsx
+++ src/common/MeditationType/MeditationType.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import styles from './MeditationType.module.css'
 
-function MeditationType({img,title,desc}) {
+function MeditationType({img,title,desc,onClick}) {
   return (
     <div>
-        <div className={styles.meditationType}>
+        <div className={styles.meditationType} onClick={onClick}>
             <img src={img} className={styles.meditationTypeImg}/>
             <div className={styles.meditationTypeContent}>
                 <h4 className={styles.title}>{title}</h4>
                 <h5 className={styles.description}>{desc}</h5>
+                
             </div>
         </div>
     </div>
