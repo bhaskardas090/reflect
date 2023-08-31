8b1171b8bf4ce44574f1bae60546a8818e7011bc
bhaskardas090 <bhaskardas090@gmail.com>
2023-08-31 21:03:11 +0530

    added change routine type images

--- src/pages/SelectRoutine.jsx
+++ src/pages/SelectRoutine.jsx
@@ -9,6 +9,10 @@ import {
 import { Link, useNavigate } from "react-router-dom";
 import useDB from "../hooks/useDB";
 import PageHeader from "../common/PageHeader/PageHeader";
+import student from "../assets/STUDENT.jpg";
+import working from "../assets/WORKING.jpg";
+import homemaker from "../assets/HOMEMAKER.jpg";
+import oldage from "../assets/OLDAGE.jpg";
 
 function SelectTask() {
   const navigate = useNavigate();
@@ -27,10 +31,14 @@ function SelectTask() {
           justifyContent: "space-around",
           alignItems: "center",
           paddingTop: "4rem",
-          // gap: '1rem',
+          gap: "1rem",
         }}
       >
-        <Link to="/" style={{ textDecoration: "none" }}>
+        <img src={student} wdth="100vw" alt="student-routine" />
+        <img src={working} alt="working-routine" />
+        <img src={homemaker} alt="homemaker-routine" />
+        <img src={oldage} alt="oldage-routine" />
+        {/* <Link to="/" style={{ textDecoration: "none" }}>
           <BoxSx name="STUDENT" color="#2196f3" activeTask={studentRoutine} />
         </Link>
         <Link to="/" style={{ textDecoration: "none" }}>
@@ -53,38 +61,38 @@ function SelectTask() {
             color="#ff9800"
             activeTask={oldAgeRoutine}
           />
-        </Link>
+        </Link> */}
       </div>
     </>
   );
 }
 
-function BoxSx({ name, color, activeTask }) {
-  const { selectRoutine } = useDB("routines");
-  const handleTask = async () => {
-    console.log(activeTask);
-    selectRoutine(activeTask);
-  };
-  return (
-    <Box
-      sx={{
-        width: "75vw",
-        height: "16vh",
-        color: "white",
-        display: "flex",
-        padding: "2rem",
-        justifyContent: "center",
-        borderRadius: "10px",
-        alignItems: "center",
-        textAlign: "center",
-        background: color,
-        cursor: "pointer",
-      }}
-      onClick={handleTask}
-    >
-      <h2>{name}</h2>
-    </Box>
-  );
-}
+// function BoxSx({ name, color, activeTask }) {
+//   const { selectRoutine } = useDB("routines");
+//   const handleTask = async () => {
+//     console.log(activeTask);
+//     selectRoutine(activeTask);
+//   };
+//   return (
+//     <Box
+//       sx={{
+//         width: "75vw",
+//         height: "16vh",
+//         color: "white",
+//         display: "flex",
+//         padding: "2rem",
+//         justifyContent: "center",
+//         borderRadius: "10px",
+//         alignItems: "center",
+//         textAlign: "center",
+//         background: color,
+//         cursor: "pointer",
+//       }}
+//       onClick={handleTask}
+//     >
+//       <h2>{name}</h2>
+//     </Box>
+//   );
+// }
 
 export default SelectTask;
